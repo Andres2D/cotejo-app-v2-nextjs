f98cb9ee8804d5429e04ffea408142173816622a
Andres2D <andres2d1997.1206@gmail.com>
2022-09-28 22:29:18 -0500

    front: Set theme configuration

--- styles/theme.conf.tsx
+++ styles/theme.conf.tsx
@@ -1,8 +1,10 @@
-export const configuration = {
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
   styles: {
     global: {
       'html, body': {
-        color: 'gray.600',
+        color: 'gray.100',
         lineHeight: 'tall',
         bg: '#1C6758'
       },
@@ -11,4 +13,32 @@ export const configuration = {
       },
     },
   },
-}
+  colors: {
+    brand: {
+      50: '#1C6758',
+      500: '#4ECB71',
+    }
+  },
+  components: {
+    Input: {
+      variants: {
+        filled: {
+          field: {
+            bg: 'white',
+            color: 'black',
+            borderRadius: '10',
+            _focusVisible: {
+              bg: 'white',
+              color: 'black'
+            },
+            _focus: {
+              borderColor: 'green.100',
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme
