fbd8a3ed88f1ea08398fd0cf7a3e59275fcddc0a
Andres2D <andres2d1997.1206@gmail.com>
2022-09-28 22:35:21 -0500

    front: Remove useless import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import theme from '../styles/theme.conf';
