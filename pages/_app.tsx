116e12bde129038fea19f53b7838239dbf031575
Andres2D <andres2d1997.1206@gmail.com>
2022-09-28 22:32:50 -0500

    front: Add the theme configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { configuration } from '../styles/theme.conf';
+
+const theme = extendTheme(configuration);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme} resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ styles/theme.conf.tsx
@@ -0,0 +1,14 @@
+export const configuration = {
+  styles: {
+    global: {
+      'html, body': {
+        color: 'gray.600',
+        lineHeight: 'tall',
+        bg: '#1C6758'
+      },
+      a: {
+        color: 'teal.500',
+      },
+    },
+  },
+}
