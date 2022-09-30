b36ebc95ed9159d44abdff90605702afe16fc192
Andres2D <andres2d1997.1206@gmail.com>
2022-09-29 20:50:29 -0500

    front: Add the session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Session } from "next-auth";
+import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react"
 import theme from '../styles/theme.conf';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ChakraProvider>
   );
 }
