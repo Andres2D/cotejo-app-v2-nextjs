758f2432a45e0e14816abf0d17a58efa8a970ed5
Andres2D <andres2d1997.1206@gmail.com>
2022-09-30 22:28:10 -0500

    server: Add the player model

---
+++ database/models/player.ts
@@ -0,0 +1,43 @@
+import { Schema, model, models } from 'mongoose';
+import { Player } from '../../interfaces/Player';
+
+const PlayerSchema = new Schema({
+    email: {
+        type: String,
+        require: [true, 'The mail is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, 'The password is required']
+    },
+    nickname: {
+        type: String,
+    },
+    name: {
+        type: String,
+        require: [true, 'The name is required']
+    },
+    number: {
+        type: Number,
+    },
+    status: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+PlayerSchema.methods.toJSON = function() {
+    let {__v, password, ...player } = this.toObject();
+    return player;
+}
+
+const Player = models['Player'] ? model<Player>('Player') : model<Player>('Player', PlayerSchema);
+
+export default Player;
