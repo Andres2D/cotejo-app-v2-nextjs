24436bb4833fdac1ef9e10cac25eebb766fcb52d
Andres2D <andres2d1997.1206@gmail.com>
2022-10-05 19:18:27 -0500

    front: Add redirect logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next';
 import { Button } from '@chakra-ui/react';
+import { getSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
+  
   return (
     <>
       <h2>Cotejo App v2</h2>
@@ -9,5 +11,16 @@ const Home: NextPage = () => {
     </>
   );
 }
+
+export const getServerSideProps = async(context: any) => {
+  const session = await getSession({ req: context.req });
+
+  return {
+    redirect: {
+      destination: session ? '/menu' : '/auth',
+      permanent: false
+    }
+  }
+};
 
 export default Home;
