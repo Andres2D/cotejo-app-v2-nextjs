0a2a851bb8c079d6983385bc3238a6d1b8014298
Andres2D <andres2d1997.1206@gmail.com>
2022-10-07 21:20:40 -0500

    front: Add rate mock and interface

---
+++ interfaces/Stats.ts
@@ -0,0 +1,4 @@
+export interface Stats {
+  label: string;
+  value: number;
+}
---
+++ mock/stats.mock.ts
@@ -0,0 +1,28 @@
+import { Stats } from '../interfaces/Stats';
+
+export const mockStats: Stats[] = [
+  {
+    label: 'Peace',
+    value: 50
+  },
+  {
+    label: 'Shoot',
+    value: 50
+  },
+  {
+    label: 'Pass',
+    value: 50
+  },
+  {
+    label: 'Dribble',
+    value: 50
+  },
+  {
+    label: 'Defense',
+    value: 50
+  },
+  {
+    label: 'Physical',
+    value: 50
+  }
+];
