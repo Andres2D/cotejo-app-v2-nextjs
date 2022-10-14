26a839212eb4134a4d255582a6812899c52eb07c
Andres2D <andres2d1997.1206@gmail.com>
2022-10-13 20:08:06 -0500

    front: Refactor player card component

--- components/profile/player-card.tsx
+++ components/profile/player-card.tsx
@@ -1,37 +1,123 @@
+import { useRef } from 'react';
 import type { NextPage } from 'next';
-import { Avatar, Divider } from '@chakra-ui/react';
+import { 
+  Avatar, 
+  Divider,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  Select,
+  Input
+} from '@chakra-ui/react';
 import Image from 'next/image';
 import styles from './player-card.module.css';
-import { Player } from '../../interfaces/Player';
-import { Rating } from '../../interfaces/Rating';
+import { IProfile } from '../../interfaces/Player';
+import { playerPositions } from '../../constants/player-positions';
+import countriesFlag from '../../constants/countries-flags';
 
 interface Props {
   className: string;
-  profile: Player;
-  overall: number;
-  flag: string;
-  stats: Rating;
+  profile: IProfile;
 }
 
-const PlayerCard: NextPage<Props> = ({profile, className, overall, flag, stats}: Props) => {
+const PlayerCard: NextPage<Props> = ({profile, className}: Props) => {
+
+  const positionRef = useRef<HTMLSelectElement>();
+
+  const { 
+    overall,
+    flag,
+    image,
+    peace,
+    shooting,
+    defense,
+    dribbling,
+    name,
+    passing,
+    physical,
+    position,
+    nationality
+  } = profile;
+
+  const updatePosition = () => {
+    console.log(positionRef.current?.value);
+  }
+
+  const updateFlag = (country: string) => {
+    console.log(country);
+  };
+
+  const positions = playerPositions.map(pos => <option key={pos} value={pos}>{pos}</option>);
+  const flags = countriesFlag.map(country => (
+    <Image
+      width={70}
+      key={country.name}
+      height={50}
+      src={country.flag}
+      alt={nationality}
+      className={styles.flag}
+      title={country.name}
+      onClick={() => updateFlag(country.name)}
+    />
+  ));
 
   return (
     <div className={`${styles.card} ${className}`}>
       <div className={styles.cardHeader}>
         <div className={styles.overall}>
           <h1 className={styles.title}>{overall}</h1>
-          <p className={styles.position}>{profile.position}</p>
+          <Popover>
+            <PopoverTrigger>
+              <p className={styles.position}>{position}</p>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>Position</PopoverHeader>
+              <PopoverBody>
+                <Select 
+                  placeholder='Select option'
+                  ref={positionRef}
+                  onChange={updatePosition}
+                >
+                  {positions}
+                </Select>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
           <Divider
             borderColor={'darks.50'}
             border='1px'
             w='80px'
             className={styles.divider}/>
-          <Image
-            width={70}
-            height={50}
-            src={flag}
-            alt={profile.nationality}
-          />
+          <Popover>
+            <PopoverTrigger>
+              <div>
+                <Image
+                    width={70}
+                    height={50}
+                    src={flag}
+                    alt={nationality}
+                    className={styles.flag}
+                  />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>Country</PopoverHeader>
+              <PopoverBody>
+                <Input placeholder='Basic usage' />
+                <div className={styles.flags}>
+                  {flags}
+                </div>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
           <Divider
             borderColor={'darks.50'}
             border='1px'
@@ -40,13 +126,13 @@ const PlayerCard: NextPage<Props> = ({profile, className, overall, flag, stats}:
         </div>
         <Avatar
           className={styles.avatar} 
-          name={profile.name}
-          src={profile.image} 
+          name={name}
+          src={image} 
           size='2xl'
         />
       </div>
       <div className={styles.cardBody}>
-        <h2 className={styles.title}>{profile.name}</h2>
+        <h2 className={styles.title}>{name}</h2>
         <Divider
           borderColor={'darks.50'}
           border='1px'
@@ -56,15 +142,15 @@ const PlayerCard: NextPage<Props> = ({profile, className, overall, flag, stats}:
         <div className={styles.rating}>
           <div className={styles.score}>
             <div className={styles.rate}>
-              <h2>{stats.peace}</h2>
+              <h2>{peace}</h2>
               <h2>PAC</h2>
             </div>
             <div className={styles.rate}>
-              <h2>{stats.shooting}</h2>
+              <h2>{shooting}</h2>
               <h2>SHO</h2>
             </div>
             <div className={styles.rate}>
-              <h2>{stats.passing}</h2>
+              <h2>{passing}</h2>
               <h2>PAS</h2>
             </div>
           </div>
@@ -77,15 +163,15 @@ const PlayerCard: NextPage<Props> = ({profile, className, overall, flag, stats}:
           />
           <div className={styles.score}>
             <div className={styles.rate}>
-              <h2>{stats.dribbling}</h2>
+              <h2>{dribbling}</h2>
               <h2>DRB</h2>
             </div>
             <div className={styles.rate}>
-              <h2>{stats.defense}</h2>
+              <h2>{defense}</h2>
               <h2>DEF</h2>
             </div>
             <div className={styles.rate}>
-              <h2>{stats.physical}</h2>
+              <h2>{physical}</h2>
               <h2>PHY</h2>
             </div>
           </div>
