aecce74814afd6c469b29e8682a0e33c21394f3c
Andres2D <andres2d1997.1206@gmail.com>
2022-10-13 20:07:27 -0500

    front: Add profile page state

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next';
+import type { NextPage} from 'next';
+import { useEffect, useState } from 'react';
+import { getSession } from 'next-auth/react';
 import { Button } from '@chakra-ui/react';
 import styles from './index.module.css';
 import PlayerCard from '../../components/profile/player-card';
 import PlayerRate from '../../components/profile/player-rate';
-import { getSession } from 'next-auth/react';
 import {  getProfile, getPlayerStats } from '../../server/player';
 import { statsMap } from '../../helpers/stats';
 import { Stats } from '../../interfaces/Stats';
-import { Player } from '../../interfaces/Player';
+import { IProfile, Player } from '../../interfaces/Player';
 import { getCountryFlag } from '../../helpers/country';
 import { Rating } from '../../interfaces/Rating';
 
@@ -18,14 +19,49 @@ interface Props {
   profile?: string;
 }
 
+const initialState: IProfile  = {
+  overall: 0,
+  position: 'CM',
+  flag: '',
+  name: '',
+  peace: 0,
+  shooting: 0,
+  passing: 0,
+  dribbling: 0,
+  defense: 0,
+  physical: 0,
+  image: '',
+  nationality: ''
+};
+
 const Profile: NextPage = ({image, name, stats, profile}: Props) => {
+
+  const [profileState, setProfileState] = useState(initialState);
+
+  useEffect(() => {
+    setProfileState({
+      overall,
+      position,
+      flag,
+      name: userName,
+      defense,
+      dribbling,
+      passing,
+      peace,
+      physical,
+      shooting,
+      image,
+      nationality
+    });
+  }, [])
+  
 
   let parsedStats: Rating;
   let parsedProfile: Player;
   let flag: string;
-  let overall = 0;
   let formattedStats: Stats[];
-  
+
+
   if(!image || !name || !stats || !profile) {
     return <p>Loading...</p>
   }
@@ -33,18 +69,30 @@ const Profile: NextPage = ({image, name, stats, profile}: Props) => {
   parsedStats = JSON.parse(stats);
   formattedStats = statsMap(parsedStats);
   parsedProfile = JSON.parse(profile);
-  overall = parsedStats.overall;
   flag = getCountryFlag(parsedProfile.nationality);
+  
+  const { 
+    position, 
+    nationality, 
+    name: userName 
+  } = parsedProfile;
+
+  const { 
+    overall, 
+    defense,
+    dribbling,
+    passing,
+    peace,
+    physical,
+    shooting
+  } = parsedStats;
   
   return (
     <section className={styles.section}>
       <div className={styles.container}>
         <PlayerCard 
           className={styles.playerCard} 
-          profile={parsedProfile}
-          overall={overall}
-          flag={flag}
-          stats={parsedStats}
+          profile={profileState}
         />
         <PlayerRate 
           className={styles.playerStats} 
@@ -67,8 +115,6 @@ export const getServerSideProps = async(context: any) => {
   const { image, name, email } = session?.user;
   const profile = await getProfile(email);
   const stats = await getPlayerStats(email);
-  
-  
   
   return {
     props: {
