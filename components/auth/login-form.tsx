6628cb1af95fda6ef3193b099fa1005da8033f48
Andres2D <andres2d1997.1206@gmail.com>
2022-10-24 20:29:12 -0500

    front: Integrate login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,15 +1,45 @@
+import { MutableRefObject, useRef } from 'react';
+import type { NextPage } from 'next';
 import {
   Button,
   FormControl,
   Input,
-  IconButton
+  IconButton,
+  useToast
 } from '@chakra-ui/react';
 import { signIn } from "next-auth/react";
 import GoogleIcon from '../../assets/svg/google.svg';
-import type { NextPage } from 'next';
 import styles from './login-form.module.css';
+import { validateEmail } from '../../helpers/validation';
 
 const LoginForm: NextPage = () => {
+
+  const emailRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const passwordRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const toast = useToast();
+
+  const onLogin = () => {
+    
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if(!email || !password || !validateEmail(email)) {
+      toast({
+        title: 'Error.',
+        description: "Invalid login, please check the form values.",
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+      emailRef.current.value = '';
+      passwordRef.current.value = '';
+      return;
+    }
+
+    signIn('credential_user', {email, password});
+
+  };
+
   return (
     <FormControl className={styles.form}>
       <Input 
@@ -20,6 +50,7 @@ const LoginForm: NextPage = () => {
         width='auto'
         size='lg'
         variant='filled'
+        ref={emailRef}
       />
       <Input 
         className={styles.input}
@@ -29,12 +60,14 @@ const LoginForm: NextPage = () => {
         width='auto'
         size='lg'
         variant='filled'
+        ref={passwordRef}
       />
       <div className={styles.actions}>
         <Button 
           className={styles.login}
           size='lg'
           colorScheme='brand'
+          onClick={onLogin}
         >
           Login
         </Button>
