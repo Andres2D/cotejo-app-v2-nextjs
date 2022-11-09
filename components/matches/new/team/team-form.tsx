477fd2c7b4f3610e426b99ecb8b593bfdb8dba68
Andres2D <andres2d1997.1206@gmail.com>
2022-11-08 19:22:15 -0500

    Implement the team shield picker

--- components/matches/new/team/team-form.tsx
+++ components/matches/new/team/team-form.tsx
@@ -1,9 +1,22 @@
-import { Image, Input, Text } from '@chakra-ui/react';
+import { 
+  Image, 
+  Input, 
+  Text,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import { useDispatch } from 'react-redux';
-import { MutableRefObject, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MutableRefObject, useRef, useState } from 'react';
+import { getAllTeams, getTeamPng } from 'empty-skull';
 import { createMatchActions } from '../../../../store/create-match.slice';
 import styles from './team-form.module.css';
+import { RootState } from '../../../../interfaces/State';
 
 interface Props {
   type?: 'home' | 'away';
@@ -12,12 +25,41 @@ interface Props {
 const TeamForm: NextPage<Props> = ({type = 'home'}: Props) => {
 
   const teamName = useRef() as MutableRefObject<HTMLInputElement>;
+  const teamShieldRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const form = useSelector((state: RootState) => state.createMatch);
   const dispatch = useDispatch();
+  const [shieldList, setShieldList] = useState(getAllTeams());
+  const defaultTeamShield = 'https://media.api-sports.io/football/teams/40.png';
 
   const updateField = () => {
-    console.log(`${type}_team_name`);
-    console.log(teamName.current.value);
-    dispatch(createMatchActions.updateInput({input: `${type}_team_name`, value: teamName.current.value}))
+    dispatch(createMatchActions.updateInput({input: `${type}_team_name`, value: teamName.current.value}));
+  }
+
+  const updateShield = (shield: string) => {
+    dispatch(createMatchActions.updateInput({input: `${type}_team_shield`, value: shield}));
+  };
+
+  const teams = shieldList.map((team) => (
+    <Image
+      width={70}
+      key={team.img}
+      height={70}
+      src={team.img}
+      alt={team.name}
+      className={styles.shield}
+      title={team.name}
+      onClick={() => updateShield(team.img)}
+    />
+  ));
+
+  const searchShield = () => {
+    if(!teamShieldRef?.current?.value) {
+      setShieldList(getAllTeams());
+      return;
+    }
+
+    const shields = getTeamPng(teamShieldRef?.current?.value);
+    setShieldList(Array.isArray(shields) ? shields : [shields]);
   }
 
   return (
@@ -31,12 +73,32 @@ const TeamForm: NextPage<Props> = ({type = 'home'}: Props) => {
         {`${type.charAt(0).toUpperCase()}${type.slice(1, type.length)}`}
       </Text>
       <div className={styles.shieldTeam}>
-        <Image
-          src={'/images/liverpool-fc-logo.png'}
-          alt='Team logo'
-          width={150}
-          height={180}
-        ></Image>
+        <Popover>
+          <PopoverTrigger>
+            <Image
+              src={form[`${type}_team_shield`] || defaultTeamShield}
+              alt='Team logo'
+              width={180}
+              height={180}
+              className={styles.selectedShield}
+            ></Image>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>Team</PopoverHeader>
+            <PopoverBody>
+              <Input 
+                  placeholder='Basic usage' 
+                  ref={teamShieldRef}
+                  onChange={searchShield}
+                />
+                <div className={styles.shields}>
+                  {teams}
+                </div>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </div>
       <Input 
         placeholder='Name of the team' 
