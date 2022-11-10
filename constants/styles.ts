ae26818733c8678ecb5f3c156b1595d2491ebb65
Andres2D <andres2d1997.1206@gmail.com>
2022-11-09 22:04:37 -0500

    Add helper to get a random color from chakra ui

---
+++ constants/styles.ts
@@ -0,0 +1,27 @@
+export const colorSchemas: string[] = [
+  // 'blackAlpha',
+  'whiteAlpha',
+  'gray',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'teal',
+  'blue',
+  'cyan',
+  'purple',
+  'pink'
+];
+
+export const colorLevels: string[] = [
+  '50',
+  '100',
+  '200',
+  '300',
+  '400',
+  '500',
+  '600',
+  '700',
+  '800',
+  '900',
+];
