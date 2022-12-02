1e714f73559cfe71f08b091e62fe68d833f6733d
Andres2D <andres2d1997.1206@gmail.com>
2022-12-02 17:47:02 -0500

    Fix id repeated issue

--- components/sign-up/sign-up-form.tsx
+++ components/sign-up/sign-up-form.tsx
@@ -65,6 +65,7 @@ const SignUpForm: NextPage<Props> = ({onSignUp}) => {
         size='lg'
         variant='filled'
         ref={emailRef}
+        id="emailRef"
       />
       <Input 
         className={styles.input}
@@ -75,6 +76,7 @@ const SignUpForm: NextPage<Props> = ({onSignUp}) => {
         size='lg'
         variant='filled'
         ref={nameRef}
+        id="nameRef"
       />
       <Input 
         className={styles.input}
@@ -85,6 +87,7 @@ const SignUpForm: NextPage<Props> = ({onSignUp}) => {
         size='lg'
         variant='filled'
         ref={passwordRef}
+        id="passwordRef"
       />
       <Input 
         className={styles.input}
@@ -95,6 +98,7 @@ const SignUpForm: NextPage<Props> = ({onSignUp}) => {
         size='lg'
         variant='filled'
         ref={confirmPasswordRef}
+        id="confirmPasswordRef"
       />
       <Button 
         onClick={signUp}
