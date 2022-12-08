d506846f009930563dbc389186c650711019a738
Andres2D <andres2d1997.1206@gmail.com>
2022-12-07 22:22:11 -0500

    Implement the team player

--- components/matches/detail/avatar/avatar.tsx
+++ components/matches/detail/avatar/avatar.tsx
@@ -8,6 +8,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from "../../../../interfaces/State";
 import { matchDetailsActions } from "../../../../store/match-details.slice";
 import styles from './avatar.module.scss'; 
+import useRequest from "../../../../hooks/use-request";
+import { IUpdateTeamPlayerRequest } from "../../../../interfaces/TeamPlayer";
 
 interface Props {
   name: string;
@@ -22,11 +24,44 @@ const AvatarMatchLayout: NextPage<Props> = ({id, name, overall, image, className
 
   const dispatch = useDispatch();
   const details = useSelector((state: RootState) => state.matchDetails);
+  const {
+    sendRequest
+  } = useRequest();
 
   const selectPlayerHandler = () => {
     dispatch(matchDetailsActions.selectPlayer({playerId: id, isAway}));
-    console.log(details.playersSelected);
-    console.log({playerId: id, isAway});
+
+    if(details.playersSelected.length > 0 && details.playersSelected[0].playerId !== id) {
+
+      let request: IUpdateTeamPlayerRequest;
+
+      if(isAway === details.playersSelected[0].isAway) {
+        const teamId =  isAway ? details.away[0].team : details.home[0].team;
+
+        request = {
+          playerOneId: details.playersSelected[0].playerId,
+          playerTwoId: id,
+          playerOneTeam: teamId,
+          playerTwoTeam: teamId
+        };
+      } else {
+        request = {
+          playerOneId: details.playersSelected[0].playerId,
+          playerTwoId: id,
+          playerOneTeam: !isAway ? details.away[0].team : details.home[0].team,
+          playerTwoTeam: isAway ? details.away[0].team : details.home[0].team
+        };
+      };
+
+      sendRequest({
+        url: '/api/team-player',
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request)
+      }, () => {});
+    }
   };
 
   return (
