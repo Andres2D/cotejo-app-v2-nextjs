9f7803a385bcda60d8174b6f4f08082ac9a2f57e
Andres2D <andres2d1997.1206@gmail.com>
2022-12-08 10:57:59 -0500

    Fix match creation process

---
+++ constants/formations-positions.ts
@@ -0,0 +1,83 @@
+export const fourTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf'
+];
+
+export const fiveTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf'
+];
+
+export const sixTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam'
+];
+
+export const sevenTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam',
+  'cm'
+];
+
+export const eightTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam',
+  'cm',
+  'cmd'
+];
+
+export const nineTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam',
+  'cm',
+  'cmd',
+  'cf',
+];
+
+export const tenTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam',
+  'cm',
+  'cf',
+  'lw',
+  'rw'
+];
+
+export const elevenTeam: string[] = [
+  'gk',
+  'lb',
+  'rb',
+  'lf',
+  'rf',
+  'cam',
+  'cm',
+  'cf',
+  'lw',
+  'rw',
+  'st'
+];
