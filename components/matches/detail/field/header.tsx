ea149d01d899de842f0eed7227102c85db671820
Andres2D <andres2d1997.1206@gmail.com>
2022-12-12 18:58:43 -0500

    Implement the stars component in the header

--- components/matches/detail/field/header.tsx
+++ components/matches/detail/field/header.tsx
@@ -4,26 +4,31 @@ import {
 } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import styles from './header.module.scss';
+import RatioStars from '../ratio-stars/ratio-stars';
 
 interface Props {
   teamName: string;
   teamShield: string;
+  teamOverall: number;
   isAway?: boolean;
 }
 
-const FieldHeader: NextPage<Props> = ({teamName, teamShield, isAway = false}) => {
+const FieldHeader: NextPage<Props> = ({teamName, teamShield, teamOverall, isAway = false}) => {
 
   return (
-    <header className={`${styles.header} ${isAway ? styles.isAway : ''}`}>
-      <Image
-        width={70}
-        height={70}
-        src={teamShield}
-        alt={teamName}
-        title={teamName}
-        m='8px'
-      />
-      <Heading size='2xl'>{teamName}</Heading>
+    <header className={`${styles.header} `}>
+      <section className={`${styles.shield} ${isAway ? styles.isAway : ''}`}>
+        <Image
+          width={70}
+          height={70}
+          src={teamShield}
+          alt={teamName}
+          title={teamName}
+          m='8px'
+        />
+        <Heading size='2xl'>{teamName}</Heading>
+      </section>
+      <RatioStars average={teamOverall} />
     </header>
   );
 }
