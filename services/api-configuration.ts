2b11bade446adaf758ec66a435ef1602359ef553
Andres2D <andres2d1997.1206@gmail.com>
2022-12-23 14:06:27 -0500

    Refactor create match request

--- services/api-configuration.ts
+++ services/api-configuration.ts
@@ -1,4 +1,5 @@
 import { create } from 'apisauce'
+import { ICreateMatchRequest } from '../interfaces/Match';
 import { UpdateProfileRequest, RegisterPlayerRequest } from '../interfaces/Player';
 import { IUpdateTeamPlayerRequest } from '../interfaces/TeamPlayer';
 
@@ -8,5 +9,6 @@ const api = create({
 });
 
 export const updatePlayer = (request: UpdateProfileRequest) => api.put('/player', request);
-export const registerPlayer = (request: RegisterPlayerRequest) => api.post('/player', request);
 export const changePlayer = (request: IUpdateTeamPlayerRequest) => api.put('/team-player', request);
+export const registerPlayer = (request: RegisterPlayerRequest) => api.post('/player', request);
+export const createMatch = (request: ICreateMatchRequest) => api.post('/match', request);
