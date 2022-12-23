cf0097bb6dbd0298af8c921d7e16e6ff02415fb3
Andres2D <andres2d1997.1206@gmail.com>
2022-12-23 13:58:07 -0500

    Refactor change player request

--- services/api-configuration.ts
+++ services/api-configuration.ts
@@ -1,5 +1,6 @@
 import { create } from 'apisauce'
 import { UpdateProfileRequest, RegisterPlayerRequest } from '../interfaces/Player';
+import { IUpdateTeamPlayerRequest } from '../interfaces/TeamPlayer';
 
 const api = create({
   baseURL: '/api',
@@ -7,5 +8,5 @@ const api = create({
 });
 
 export const updatePlayer = (request: UpdateProfileRequest) => api.put('/player', request);
-
 export const registerPlayer = (request: RegisterPlayerRequest) => api.post('/player', request);
+export const changePlayer = (request: IUpdateTeamPlayerRequest) => api.put('/team-player', request);
