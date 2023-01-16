9e1078ce46a572862e71156cda34870902d9ee8a
Andres2D <andres2d1997.1206@gmail.com>
2023-01-15 22:35:07 -0500

    Add leave match option from match details page

--- components/matches/detail/match-detail.tsx
+++ components/matches/detail/match-detail.tsx
@@ -1,19 +1,76 @@
+import { Button, useDisclosure, useToast } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import { useSelector } from 'react-redux';
+import { useMutation } from 'react-query';
+import { useRouter } from 'next/router';
 import { RootState } from '../../../interfaces/State';
 import ChangePlayerModal from './avatar/change-player.modal';
 import FieldLayout from './field/field';
 import styles from './match-detail.module.scss';
+import LeaveIcon from '../../../assets/svg/leave.svg';
+import ModalAlert from '../../layout/modal-alert';
+import { leaveMatch } from '../../../services/api-configuration';
 
 const MatchDetailsLayout: NextPage = () => {
   const matchDetails = useSelector((state: RootState) => state.matchDetails);
+  const { 
+    isOpen: leaveMatchModalIsOpen, 
+    onOpen: leaveMatchModalOnOpen,
+    onClose: leaveMatchModalOnClose 
+  } = useDisclosure();
+  const router = useRouter();
+  const toast = useToast();
+
+  const { mutate: mutateLeaveMatch } = useMutation(leaveMatch, {
+    onSuccess: async (response) => {
+      if(response.ok) {
+        router.push('/matches', undefined, {  })
+      } else {
+        toast({
+          title: 'Leave match',
+          description: "Something went wrong, please try again later.",
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+        leaveMatchModalOnClose();
+      }
+    },
+    onError: () => {
+      // TODO: handle error
+    }
+  });
+
+  const handleLeaveMatch = () => {
+    mutateLeaveMatch(matchDetails.match._id);
+  };
 
   return (
-    <section className={styles.fields}>
-      <ChangePlayerModal />
-      <FieldLayout team={matchDetails.home.slice(0, 11)} />
-      <FieldLayout team={matchDetails.away.slice(0, 11)} isAway />
-    </section>
+    <>
+      <section className={styles.details}>
+        <div className={styles.fields}>
+          <ChangePlayerModal />
+          <FieldLayout team={matchDetails.home} />
+          <FieldLayout team={matchDetails.away} isAway />
+        </div>
+        <Button 
+          colorScheme='telegram'
+          className={styles.leaveButton}
+          rightIcon={<LeaveIcon />}
+          onClick={leaveMatchModalOnOpen}
+        >
+            Leave match
+        </Button>
+      </section>
+      <ModalAlert 
+        isOpen={leaveMatchModalIsOpen} 
+        onClose={leaveMatchModalOnClose}
+        onContinue={handleLeaveMatch}
+        title='Leave match'
+        description={`Are you sure? You would request to a team mate to add you again afterwards`}
+        continueLabel='Leave match'
+      />
+    </>
   );
 };
 
