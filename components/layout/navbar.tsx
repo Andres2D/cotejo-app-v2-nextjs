44a278a22ab264a45831d6c2591cf363daa06833
Andres2D <andres2d1997.1206@gmail.com>
2023-02-17 20:55:16 -0500

    Update version

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -99,7 +99,7 @@ const Navbar: NextPage = () => {
             >
               Log out
             </Button>
-            <p>Version 2.5.0</p>
+            <p>Version 2.5.1</p>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
