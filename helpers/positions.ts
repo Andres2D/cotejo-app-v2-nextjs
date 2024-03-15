c6d3c525fc8825327d13a9902c1f1310e376b49f
Andres2D <andres2d1997.1206@gmail.com>
2024-03-15 19:52:57 -0400

    Validate position configuration

--- helpers/positions.ts
+++ helpers/positions.ts
@@ -2,12 +2,13 @@ import { orderRule } from "../constants/player-positions";
 import { IFullPlayer } from "../interfaces/Player";
 
 export const sortPlayerPositions = (playersArr: IFullPlayer[]) => {
+  console.log(playersArr);
   return [...playersArr].sort((a, b) => {
-    if(orderRule[a.position.toUpperCase()] < orderRule[b.position.toUpperCase()]) {
+    if(orderRule[a.position?.toUpperCase() ?? 'CM'] < orderRule[b.position?.toUpperCase() ?? 'CM']) {
       return -1;
     }
 
-    if(orderRule[a.position.toUpperCase()] > orderRule[b.position.toUpperCase()]) {
+    if(orderRule[a.position?.toUpperCase() ?? 'CM'] > orderRule[b.position?.toUpperCase() ?? 'CM']) {
       return 1;
     }
 
