d45772d1c0e28925f334c8f2d77468fc67173a7d
Andres2D <andres2d1997.1206@gmail.com>
2024-03-18 00:29:51 -0400

    Remove console log

--- helpers/positions.ts
+++ helpers/positions.ts
@@ -2,7 +2,6 @@ import { orderRule } from "../constants/player-positions";
 import { IFullPlayer } from "../interfaces/Player";
 
 export const sortPlayerPositions = (playersArr: IFullPlayer[]) => {
-  console.log(playersArr);
   return [...playersArr].sort((a, b) => {
     if(orderRule[a.position?.toUpperCase() ?? 'CM'] < orderRule[b.position?.toUpperCase() ?? 'CM']) {
       return -1;
